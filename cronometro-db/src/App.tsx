a2ecc64983d684080d8e7db31fe46e0cf7b42c1c
Diego Bran <diegobran@umes.edu.gt>
2025-09-23 22:13:16 -0600

    Add Tailwind and DaisyUI, implement stopwatch UI
    Integrated Tailwind CSS and DaisyUI for styling, replacing custom CSS. Updated App.tsx to implement a basic stopwatch UI with hour, minute, and second display, and added LAP and STOP buttons. Removed unused code and simplified the layout.

--- cronometro-db/src/App.tsx
+++ cronometro-db/src/App.tsx
@@ -1,20 +1,60 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      
+      setHour((prevHour) => {
+        if (prevHour === 23) {
+          return 0;
+        } else {
+          return prevHour;
+        }
+      });
+
+      setMinute((prevMinute) => {
+        if (prevMinute === 59) {
+          setHour((prevHour) => (prevHour === 23 ? 0 : prevHour + 1));
+          return 0;
+        } else {
+          return prevMinute;
+        }
+      });
+
+      setSecond((prevSecond) => {
+        if (prevSecond === 59) {
+          setMinute((prevMinute) => (prevMinute === 59 ? 0 : prevMinute + 1));
+          return 0;
+        } else {
+          return prevSecond + 1;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  } , []);
   return (
     <>
-      <h1>CRONÓMETRO</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>CRONÓMETRO DB</h1>
+      <div className="cronometro-container">
+        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+            <span className="time, countdown font-mono text-5xl">
+              {hour.toString().padStart(2, '0')}:{minute.toString().padStart(2, '0')}:{second.toString().padStart(2, '0')} 
+            </span>
+        </div>
+        
+        <div className="card-actions justify-end">
+            <button className="btn btn-primary">LAP</button>
+            <button className="btn btn-primary">STOP</button>
+        </div>
       </div>
     </>
   )
